1c85e84c1b3a489100b0882ab6866518c0a073d7
SsmuelHG <erik_1320114042@uptecamac.edu.mx>
2021-09-19 00:44:13 -0500

    PROGRAMA PARA GUARDAR INFORMACION

---
+++ ARREGLOS_15_09_2021/main.js
@@ -0,0 +1,54 @@
+var elementos = [];
+
+var colore = ["red", "green", "blue", "yellow"];
+
+var humano = {
+    "informacion": {
+        "nombre":"ERIK SAMUEL",
+        "Edad":19,
+        "Carrera": "Ingenieria en Sofware"
+    },
+    "musica": {
+        "generos":{
+            "rock": {
+                "top 5 canciones":{
+                    "Uno":"I can´t Get No",
+                    "Dos":"November Rain",
+                    "Tres":"Smells Like Teen Spirit",
+                    "Cuatro":"We Wiil Rock You",
+                    "Cinco":"Sweet Child O´Mine",
+                    "Calificacion":{
+                        "I can´t Get No":"*****",
+                        "November Rain":"***",
+                        "Smells Like Teen Spirit":"***",
+                        "We Wiil Rock You":"***",
+                        "Sweet Child O´Mine":"****",
+
+
+                    }
+
+                }
+            },
+            "Hobbies":{
+                "uno":"Jugar Videojuegos",
+                "dos":"Hacer ejercicio",
+                "tres":"Ver peliculas",
+                "cuatro":"Tocar la guitarra",
+                "cinco":"Programar",
+            },
+            "Bnada":{
+                "uno":"Critian Nodal",
+                "dos":"Ariel",
+                "tres":"Farsante",
+                "cuatro":"Grupon Firme",
+                "cinco":"Regional",
+            }
+         
+
+            
+        }
+    }
+};
+
+console.log(humano);
+
